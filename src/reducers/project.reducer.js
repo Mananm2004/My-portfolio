ece9bd330ce5614a3e948061b1b5595b1f7400b9
Manan Malik <mananm359@gmail.com>
2025-03-31 10:15:17 +0530

    All assets moved from src to public

--- src/reducers/project.reducer.js
+++ src/reducers/project.reducer.js
@@ -4,7 +4,7 @@ const initialState = [
   {
     title: "CampusRide",
     development: "Full-Stack Project ",
-    img: "src/assets/CampusRide.png",
+    img: "/CampusRide.png",
     description: `Engineered a real-time ride-hailing platform for campus students to efficiently book e-rickshaws, improving
     campus mobility.Integrated Google Maps API for real-time location tracking, optimized route planning, and accurate fare
     estimation.Enhanced real-time communication between students and e-rickshaw drivers, reducing average wait times by
@@ -32,7 +32,7 @@ const initialState = [
     shows with a 98% API response accuracy.
    Optimized search and filtering algorithms, boosting search result relevancy by 25% and enhancing the user
 experience.`,
-    img: "src/assets/MovieApp.png",
+    img: "/MovieApp.png",
     liveLink: "https://movie-app-n8ci.vercel.app/",
     gitRepo: "https://github.com/Mananm2004/MovieApp.git",
     techStack: ["ReactJS", "Redux", "Tailwind CSS", "Context-API"],
@@ -42,7 +42,7 @@ experience.`,
     development: "Full-Stack Project ",
     description: `Developed a developer networking platform that connects programmers based on skills, interests, and
     collaboration needs.Designed a React-based frontend with Tailwind CSS, ensuring a modern, responsive, and user-friendly inter-face.Secured user authentication with JWT-based authentication and role-based access control, safeguarding user data.`,
-    img: "src/assets/DevTinder.png",
+    img: "/DevTinder.png",
     flexDirection: "flex-row-reverse",
     bgColor: "[#58C2F9]",
     liveLink: "https://dev-tinder-frontend-nine.vercel.app/",
